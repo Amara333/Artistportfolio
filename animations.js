c60babca8e38526bc621c86676b9b95ef56766df
Amara333 <Amara.catelyne@gmail.com>
2021-06-07 04:03:08 +0000

    Adding Menu & organsing code

---
+++ animations.js
@@ -0,0 +1,22 @@
+
+
+// (function() {
+
+// console.log("hello")
+
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+
+// function drawRectangle(rec_x, rec_y, rec_width, rec_height, rec_colour) { 
+//     ctx.beginPath();
+//     ctx.rect(rec_x, rec_y, rec_width, rec_height);
+//     ctx.fillStyle = rec_colour;
+//     ctx.fill(); 
+// } 
+
+
+// drawRectangle(100, 100, 50, 10, "green"); 
+// drawRectangle(300, 300, 100, 50, "blue");
+// drawRectangle(100, 300, 5, 70, "purple"); 
+
+// })();
